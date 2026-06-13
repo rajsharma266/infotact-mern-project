98fd3adb34b8c688b8720d641f3f66be335cf10b
Supriya Nayak <nayakjhunu40@gmail.com>
2026-06-13 18:30:50 +0530

    created Channel List Component

--- client/src/components/Workspace/ChannelList.tsx
+++ client/src/components/Workspace/ChannelList.tsx
@@ -1,7 +1,20 @@
 function ChannelList() {
+  const channels = ["general", "frontend", "backend", "ui-design"];
+
   return (
-    <div className="w-64 bg-slate-800 text-white p-4">
-      Channels
+    <div className="p-4">
+      <h3 className="text-slate-400 mb-3 uppercase text-sm">Channels</h3>
+
+      <div className="space-y-2">
+        {channels.map((channel) => (
+          <div
+            key={channel}
+            className="px-3 py-2 rounded-lg hover:bg-slate-800 cursor-pointer transition"
+          >
+            # {channel}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
