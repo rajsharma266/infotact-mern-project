8466522b5e8b2e6a70d4db59753779f8c17d6722
swastiswagat <swagatswasti@gmail.com>
2026-06-17 18:12:34 +0530

    Updated for Testing purpose only

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
-import Workspace from "./pages/Workspace";
+// import Workspace from "./pages/Workspace";
 // import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
-  return <Workspace />;
+  // return <Workspace />;
   // return <Login />;
+  return <Signup />;
 } 
 
 export default App;
