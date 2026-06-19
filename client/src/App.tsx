4a982e9ed876217990cce19ea420d261d33de704
swastiswagat <swagatswasti@gmail.com>
2026-06-19 22:01:39 +0530

    handeled routing operation for testing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,20 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import Workspace from "./pages/Workspace";
-// import Login from "./pages/Login";
-// import Signup from "./pages/Signup";
 
 function App() {
-  return <Workspace />;
-  // return <Login />;
-  // return <Signup />;
-} 
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
+
+      <Route path="/login" element={<Login />} />
+
+      <Route path="/signup" element={<Signup />} />
+      
+      <Route path="/workspace" element={<Workspace />} />
+    </Routes>
+  );
+}
 
 export default App;
